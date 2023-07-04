a54bf0a126c783148002e0c900e0af80a2c8f5b4
vector2927 <mr.soeminthu29@gmail.com>
2023-07-05 01:03:10 +0630

    Update script.js

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+       var chosenCategoryShortName = function chooseRandomCategory();
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,13 +117,19 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+       var homeHtmlToInsertIntoMainPage = function (string, propName, propValue) {
+        var propToReplace = "{{" + propName + "}}";
+        string = string
+          .replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+      };
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+       
+};
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
